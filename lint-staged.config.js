2593aa0823f252a18ebba61bd6b0f380879c2cb1
jfatras <jjcfatras@gmail.com>
2025-01-22 14:10:42 -0500

    chore: add --fix to linting in lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,12 +3,12 @@ module.exports = {
     (files) => `nx format:write --files=${files.join(",")}`,
   ],
   "{apps}/**/*.{js,cjs,mjs,jsx}": [
-    (files) => `nx affected:lint --files=${files.join(",")}`,
+    (files) => `nx affected:lint --fix --files=${files.join(",")}`,
     (files) => `nx format:write --files=${files.join(",")}`,
   ],
   "{apps}/**/*.{ts,cts,mts,tsx}": [
     (files) => `nx affected --target=compile --files=${files.join(",")}`,
-    (files) => `nx affected:lint --files=${files.join(",")}`,
+    (files) => `nx affected:lint --fix --files=${files.join(",")}`,
     (files) => `nx format:write --files=${files.join(",")}`,
   ],
 };

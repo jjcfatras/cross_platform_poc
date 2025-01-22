98a5fdfa6264ed2a6dd68092a0d19a55d18b4617
jfatras <jjcfatras@gmail.com>
2025-01-22 14:37:13 -0500

    chore: add libs directory

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  "{apps,libs}/**/*.{css,json,yml,svg}": [
+  "{apps,libs,tools}/**/*.{css,json,yml,svg}": [
     (files) => `nx format:write --files=${files.join(",")}`,
   ],
-  "{apps,libs}/**/*.{js,cjs,mjs,jsx}": [
+  "{apps,libs,tools}/**/*.{js,cjs,mjs,jsx}": [
     (files) => `nx affected:lint --fix --files=${files.join(",")}`,
     (files) => `nx format:write --files=${files.join(",")}`,
   ],
-  "{apps,libs}/**/*.{ts,cts,mts,tsx}": [
+  "{apps,libs,tools}/**/*.{ts,cts,mts,tsx}": [
     (files) => `nx affected --target=compile --files=${files.join(",")}`,
     (files) => `nx affected:lint --fix --files=${files.join(",")}`,
     (files) => `nx format:write --files=${files.join(",")}`,

895da0a42c6664a9fca797218e01e61e4ad452cd
jfatras <jjcfatras@gmail.com>
2025-01-22 13:28:47 -0500

    feat: setup git hook

--- apps/web/src/app/api/hello/route.ts
+++ apps/web/src/app/api/hello/route.ts
@@ -1,3 +1,4 @@
 export async function GET(request: Request) {
+  console.log(await request.json());
   return new Response("Hello, from API!");
 }
---
+++ lint-staged.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "{apps}/**/*.{ts,cts,mts,tsx}": [
+    (files) => `nx affected --target=compile --files=${files.join(",")}`,
+    (files) => `nx affected:lint --files=${files.join(",")}`,
+    (files) => `nx format:write --files=${files.join(",")}`,
+  ],
+  "{apps}/**/*.{js,cjs,mjs,jsx}": [
+    (files) => `nx affected:lint --files=${files.join(",")}`,
+    (files) => `nx format:write --files=${files.join(",")}`,
+  ],
+  "{apps}/**/*.{css,json,yml,svg}": [
+    (files) => `nx format:write --files=${files.join(",")}`,
+  ],
+};

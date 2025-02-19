4fe87ec93c4b8b1c9a5e4c0eaf36cb52cc0d9bab
jfatras <jjcfatras@gmail.com>
2025-02-18 21:22:25 -0500

    feat: get swr and axios working

--- apps/web2/src/pages/index.tsx
+++ apps/web2/src/pages/index.tsx
@@ -1,9 +1,28 @@
+import useSWR from "swr";
+
+import { getArtistAlbums, getAuthToken } from "~/libs/backend/src/services";
 import { Card, Container, Grid, Screen, Text } from "~/libs/ui/src/components";
 
 export default function Index() {
+  const { data: tokenData } = useSWR("/api/token", getAuthToken);
+  const { data: albumData } = useSWR(
+    tokenData
+      ? {
+          artistId: "3WrFJ7ztbogyGnTHbHJFl2",
+          authToken: tokenData?.access_token,
+          tokenType: tokenData?.token_type,
+          url: "/artist/:id/album",
+        }
+      : null,
+    getArtistAlbums,
+  );
+
+  console.log(JSON.stringify({ tokenData }, null, "\t"));
+  console.log(JSON.stringify({ albumData }, null, "\t"));
+
   return (
-    <Screen>
-      <Container hasHeader>
+    <Screen hasHeader>
+      <Container>
         <Grid>
           {Array.from({ length: 20 }, (_, index) => (
             <Card key={index}>
